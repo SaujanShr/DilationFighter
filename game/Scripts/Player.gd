b135b73e41a88af7018c138e118f737211f54ad5
SaujanShr <99749301+SaujanShr@users.noreply.github.com>
2022-03-15 01:36:58 +0000

    added stuff

--- game/Scripts/Player.gd
+++ game/Scripts/Player.gd
@@ -1,43 +1,70 @@
 extends KinematicBody2D
 
 onready var state_machine = $AnimationTree.get("parameters/playback")
-var run_speed = 100
-var velocity = Vector2.ZERO
+
+export var run_speed = 100
+export var jump_force = 800
+export var gravity = 50
+export var terminal_v = 100
+var velocity_y = 0
+var motion
+
+var jump = false
+var attacking = false
 
 func _ready():
 	pass
 
 func get_input():
 	var current = state_machine.get_current_node()
-	velocity = Vector2.ZERO
+	var direction = 0
+	motion = 0
 	
 	#attacks
 	if Input.is_action_just_pressed("attack_1"):
 		state_machine.travel("Attack1")
-		return
+		attacking = true
 	if Input.is_action_just_pressed("attack_2"):
 		state_machine.travel("Attack2")
-		return
+		attacking = true
 	
-	#left and right movement
+	#left and right and jump movement
 	if Input.is_action_pressed("move_left"):
-		move_in_x(-1)
+		direction = -1
 	if Input.is_action_pressed("move_right"):
-		move_in_x(1)
-	velocity = velocity.normalized() * run_speed
-	
-	#if moving, run (only run, no jumps or anything or recovery time etc)
-	if velocity.length() == 0:
-		state_machine.travel("Idle")
-	elif velocity.length() > 0:
-		state_machine.travel("Run")
+		direction = 1
+	if Input.is_action_just_pressed("jump"):
+		jump = true
+	direction_in_x(direction)
+	motion = direction * run_speed
 
 func _physics_process(delta):
 	get_input()
-	velocity = move_and_slide(velocity)
+	#if attacking, dont do other animations
+	if attacking == false:
+		if motion == 0:
+			state_machine.travel("Idle")
+		elif abs(motion) > 0:
+			state_machine.travel("Run")
+	else:
+		attacking = true
+		
+	motion = move_and_slide(Vector2(motion, velocity_y), Vector2(0, -1))
+	
+	#gravity and jumping
+	var grounded = is_on_floor()
+	velocity_y += gravity
+	if grounded and jump:
+		velocity_y = -jump_force
+	if grounded and velocity_y >= 5:
+		velocity_y = 5
+	if velocity_y > terminal_v:
+		velocity_y = terminal_v
+	
+	jump = false
 
-func move_in_x(direction_vector):
-	velocity.x = direction_vector
-	$Sprite.scale.x = direction_vector
-	$CollisionPolygon2D.scale.x = direction_vector
-	$AttackArea.scale.x = direction_vector
+func direction_in_x(direction):
+	if not direction == 0:
+		$Sprite.scale.x = direction
+		$CollisionPolygon2D.scale.x = direction
+		$AttackArea.scale.x = direction
